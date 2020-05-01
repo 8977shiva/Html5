381586af0042eb2c65f27ffffafd68058f6dc794
8977shiva <b.skreddy1997@gmail.com>
2020-05-01 20:56:42 +0530

    webStorage

---
+++ webStorage/form.js
@@ -0,0 +1,33 @@
+var webStorage=localStorage;
+
+$(document).ready(()=>{
+    $('#sessionStorage').click(function (e) { 
+        //   console.log("clicked")
+           if(this.checked){
+            webStorage=sessionStorage;
+           }
+          return webStorage;
+      });
+
+    $('input:submit[value=save]').click((e)=>{
+        e.preventDefault();
+        webStorage.setItem('flag', 'set')
+     
+        var data=$("#signUp").serializeArray();
+       $.each(data,(i,obj)=>{
+           webStorage.setItem(obj.name, obj.value)
+       })
+        
+    })
+    if(webStorage.getItem("flag")=='set'){
+        $('section').before("<h3>the form has been save</h3>")
+            var data=$('#signUp').serializeArray();
+            $.each(data, (i,obj)=>{
+                $("[name='"+obj.name+"']").val(webStorage.getItem(obj.name));
+            })
+    }
+})
+
+
+
+
---
+++ webStorage/script.js
@@ -0,0 +1,15 @@
+// localStorage.setItem('name', 'bob');
+// console.log("getItem",localStorage.getItem('name'))
+// console.log("key",localStorage.key(0))
+// console.log("length",localStorage.length)
+// localStorage.clear();
+// localStorage.setItem('lastname', 'shiva');
+// localStorage.removeItem('lastname');
+
+
+/* -------------------------------------------------------------------------- */
+/*                               sessionStorage                               */
+/* -------------------------------------------------------------------------- */
+
+
+sessionStorage.setItem('firstname','shiva')
